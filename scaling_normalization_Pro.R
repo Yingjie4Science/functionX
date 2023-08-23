5321d4661074e0fc9b8d2af88cd7c340be3bfa3d
Yingjie Li <yingjieli.edu@gmail.com>
2023-08-23 13:32:53 -0700

    add func_norm_minmax

--- scaling_normalization_Pro.R
+++ scaling_normalization_Pro.R
@@ -33,3 +33,11 @@ func_z_scaling <- function(data, var){
     dplyr::mutate_at(vars(var), ~ base::scale(., center = TRUE, scale = TRUE))
   return(d)
 }
+
+
+###' If we want to scale between some bounded arbitrary set of values [a, b]
+###'  Mean Normalisation
+func_norm_minmax <- function(x){
+  (x- mean(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+}
+
