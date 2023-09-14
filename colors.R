843d269726f4ac11c224bacc5b02c7e4f2a85bd2
Yingjie Li <yingjieli.edu@gmail.com>
2023-09-14 11:21:35 -0700

    colors for income groups and world regions

---
+++ colors.R
@@ -0,0 +1,13 @@
+
+
+name_income4 <- 
+  c('Low income', 'Lower middle income', 'Upper middle income', 'High income')
+color_income4 <- 
+  c('#aa2626', '#d36f8e', '#7b8ad0', '#1b607d')
+
+
+name_region7 <- 
+  c('Middle East & North Africa', 'Europe & Central Asia', 'East Asia & Pacific', 
+    'Sub-Saharan Africa', 'Latin America & Caribbean', 'South Asia', 'North America')
+color_region7 <- 
+  c('#9E2F59', '#22789D', '#FFBF24', '#E56666', '#509A9A', '#83B35C', '#ED8E3F')
