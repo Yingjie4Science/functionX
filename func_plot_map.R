5c015559e9e4717f2001d1b177add5bc7bc79017
Yingjie Li <yingjieli.edu@gmail.com>
2023-12-25 14:23:05 -0800

    US map

--- func_plot_map.R
+++ func_plot_map.R
@@ -38,3 +38,44 @@ plot_tmap <- function(data, var = 'value_rel', color_direction = 1, filename_pos
   tmap_save(tm = p, filename = fname, width=7, height=2.8, units="in", dpi = 200)
   
 }
+
+
+
+## plot US map ===========================================================================
+net_migration <- get_estimates(geography = "county",
+                               variables = "RNETMIG",
+                               year = 2022,
+                               geometry = TRUE,
+                               resolution = "20m") %>%
+  shift_geometry()
+
+order = c("-15 and below", "-15 to -5", "-5 to +5", "+5 to +15", "+15 and up")
+
+net_migration <- net_migration %>%
+  mutate(groups = case_when(
+    value > 15 ~ "+15 and up",
+    value > 5 ~ "+5 to +15",
+    value > -5 ~ "-5 to +5",
+    value > -15 ~ "-15 to -5",
+    TRUE ~ "-15 and below"
+  )) %>%
+  mutate(groups = factor(groups, levels = order))
+
+state_overlay <- states(
+  cb = TRUE,
+  resolution = "20m"
+) %>%
+  filter(GEOID != "72") %>%
+  shift_geometry()
+
+ggplot() +
+  geom_sf(data = net_migration, aes(fill = groups, color = groups), size = 0.1) +
+  geom_sf(data = state_overlay, fill = NA, color = "black", size = 0.1) +
+  scale_fill_brewer(palette = "PuOr", direction = -1) +
+  scale_color_brewer(palette = "PuOr", direction = -1, guide = FALSE) +
+  coord_sf(datum = NA) +
+  theme_minimal(base_family = "Roboto") +
+  labs(title = "Net migration per 1000 residents by county",
+       subtitle = "US Census Bureau 2022 Population Estimates",
+       fill = "Rate",
+       caption = "Data acquired with the R tidycensus package | @kyle_e_walker")
